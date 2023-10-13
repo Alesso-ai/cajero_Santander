934706510d2e9a544e39e94afcba456f93bae195
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-10-13 12:54:33 +0200

    Actualizado

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const mensaje = document.getElementById("mensaje");
+  const saldo = document.getElementById("saldo");
+  const importe = document.getElementById("importe");
+  const retirar = document.getElementById("retirar");
+  const depositar = document.getElementById("depositar");
 
+  retirar.addEventListener("click", function () {
+    const cantidad = parseFloat(importe.value);
+    if (isNaN(cantidad) || cantidad <= 0 || cantidad > saldo) {
+      mensaje.textContent = "Importe no valido";
+    } else {
+      saldo = saldo - cantidad;
+      saldo();
+      mensaje.textContent ="retirado: ${cantidad}"; 
+    }
     
 
-    const mensaje = document.getElementById("mensaje");
-    const
-
-    
 
 
-  
+
+
+
+
+  });
 });
